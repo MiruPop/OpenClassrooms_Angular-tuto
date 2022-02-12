09532b88b533a9379af0ec46342d25863138c87a
Miruna Popescu <miru.popescu@yahoo.fr>
2022-02-12 17:22:17 +0100

    fin routage et fin cours

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapsService } from '../services/face-snaps.service';
 
@@ -12,7 +13,8 @@ export class FaceSnapComponent implements OnInit {
   @Input() faceSnap!: FaceSnap;
   buttonText!: string;
 
-  constructor(private faceSnapsService : FaceSnapsService) {}
+  constructor(private faceSnapsService : FaceSnapsService,
+    private router : Router) {}
 
   ngOnInit(): void {
 
@@ -28,5 +30,12 @@ export class FaceSnapComponent implements OnInit {
       this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'unsnap')
       this.buttonText = 'Oh Snap!';
     }
+  }
+
+  // les gabarits sont délimités par (` `)  au lieu des apostrophes doubles ou simples.
+  // Ils peuvent contenir des espaces réservés (placeholders).
+  // Ces espaces sont indiqués par le signe dollar ($) et des accolades (${expression})
+  onViewFaceSnap() {
+    this.router.navigateByUrl(`facesnaps/${this.faceSnap.id}`);
   }
 }
---
+++ src/app/single-face-snap/single-face-snap.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FaceSnap } from '../models/face-snap.model';
+import { FaceSnapsService } from '../services/face-snaps.service';
+
+@Component({
+  selector: 'app-single-face-snap',
+  templateUrl: './single-face-snap.component.html',
+  styleUrls: ['./single-face-snap.component.scss']
+})
+export class SingleFaceSnapComponent implements OnInit {
+
+  faceSnap!: FaceSnap;
+  buttonText!: string;
+
+  constructor(private faceSnapsService: FaceSnapsService,
+    private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.buttonText = 'Oh Snap!';
+    // typecast du paramètre 'id', from String to number, en ajoutant le '+'
+    const faceSnapId = +this.route.snapshot.params['id'];
+    this.faceSnap = this.faceSnapsService.getFaceSnapById(faceSnapId);
+  }
+
+  onSnap() {
+    if (this.buttonText === 'Oh Snap!') {
+      this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'snap')
+      this.buttonText = 'Oups, unSnap!';
+    }
+    else {
+      this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'unsnap')
+      this.buttonText = 'Oh Snap!';
+    }
+  }
+
+}
